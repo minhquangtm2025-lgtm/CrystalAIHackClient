b15daa1b67f62b3ad6d1e7d7ecd762232f967a34
minhquangtm2025-lgtm <minhquangtm2025@gmail.com>
2026-05-10 09:56:29 +0700

    Add files via upload

---
+++ CrystalAIHackClient/src/main/java/com/crystalai/CrystalAI.java
@@ -0,0 +1,10 @@
+package com.crystalai;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CrystalAI implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Main mod entrypoint kept intentionally minimal.
+    }
+}
